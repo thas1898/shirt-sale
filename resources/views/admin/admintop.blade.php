08878a16cbcc0f3030b389eba6f41c85ccb1e880
thas1898 <mca553@rajagiri.edu>
2021-05-23 22:17:45 +0530

    new

--- resources/views/admin/admintop.blade.php
+++ resources/views/admin/admintop.blade.php
@@ -6,6 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
 
